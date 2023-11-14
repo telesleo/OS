bbe98ce083bbc7366382b2a336a63667ae59cfe9
leo <leonardoandrei41@gmail.com>
2023-11-14 11:06:18 -0300

    Implement createDirectory function

--- src/utils/fileSystem.js
+++ src/utils/fileSystem.js
@@ -35,3 +35,29 @@ export function resolvePath(storage, path) {
 
   return current;
 }
+
+export function createDirectory(storage, setStorage, name, path) {
+  if (!isPathValid(storage, path)) {
+    return 'Invalid directory';
+  }
+
+  const parts = getPathParts(path);
+  let current = storage.content;
+
+  for (let index = 0; index < parts.length; index += 1) {
+    const part = parts[index];
+    current = current[part].content;
+  }
+
+  if (current[name] !== undefined) {
+    return 'Directory already exists';
+  }
+
+  current[name] = {
+    type: 'directory',
+    content: {},
+  };
+
+  setStorage({ ...storage });
+  return `Directory ${name} created at path ${path}`;
+}
