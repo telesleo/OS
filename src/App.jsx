a188b2ad21f53c61758df4dcfd9b131c59cbcefe
leo <leonardoandrei41@gmail.com>
2023-11-17 15:41:27 -0300

    Add class square to App

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Main from './Pages/Main';
 import Terminal from './components/Terminal';
+import './App.css';
 
 function App() {
   const [storage, setStorage] = useState({
